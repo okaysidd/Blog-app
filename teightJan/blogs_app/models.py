9f03dd40762633873215ec813ef3e2eed0bb98b2
Siddhartha Sehgal (Robert Bosch Engineering and) <v-sisehg@microsoft.com>
2019-02-01 16:25:39 +0530

    New changes

--- teightJan/blogs_app/models.py
+++ teightJan/blogs_app/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.urls import reverse
 from users_app.models import Author_model
+import datetime
 
 
 # Create your models here.
 class Post_model(models.Model):
     author = models.ForeignKey(Author_model, on_delete=models.CASCADE)
     title_original = models.CharField(max_length=200)
-    title_edited = models.CharField(max_length=200, blank=True, null=True)
+    title_edited = models.CharField(max_length=200, blank=True, null=True, default=None)
     body_original = models.TextField()
-    body_edited = models.TextField(blank=True, null=True)
+    body_edited = models.TextField(blank=True, null=True, default=None)
     created_on = models.DateTimeField(auto_now_add=True)
-    published_on = models.DateTimeField(blank=True, null=True)
-    modified_on = models.DateTimeField(blank=True, null=True)
+    published_on = models.DateTimeField(default=datetime.datetime.now())
+    modified_on = models.DateTimeField(default=datetime.datetime.now())
 
     def get_absolute_url(self):
         return reverse('blogs:post', kwargs={'pk':self.pk})
