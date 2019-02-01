732d69ea048bf03003bcec39bc412fd81967cbf3
Siddhartha Sehgal (Robert Bosch Engineering and) <v-sisehg@microsoft.com>
2019-02-01 19:22:55 +0530

    New changes to the blogs models

--- teightJan/blogs_app/models.py
+++ teightJan/blogs_app/models.py
@@ -2,21 +2,25 @@ from django.db import models
 from django.urls import reverse
 from users_app.models import Author_model
 import datetime
+from django.utils import timezone
 
 
 # Create your models here.
 class Post_model(models.Model):
     author = models.ForeignKey(Author_model, on_delete=models.CASCADE)
     title_original = models.CharField(max_length=200)
-    title_edited = models.CharField(max_length=200, blank=True, null=True, default=None)
+    title_edited = models.CharField(max_length=200, default="")
     body_original = models.TextField()
-    body_edited = models.TextField(blank=True, null=True, default=None)
-    created_on = models.DateTimeField(auto_now_add=True)
-    published_on = models.DateTimeField(default=datetime.datetime.now())
-    modified_on = models.DateTimeField(default=datetime.datetime.now())
+    body_edited = models.TextField(default="")
+    created_on = models.DateTimeField(default=timezone.now())
+    modified_on = models.DateTimeField(blank=True, null=True)
+    published_on = models.DateTimeField(blank=True, null=True)
 
     def get_absolute_url(self):
         return reverse('blogs:post', kwargs={'pk':self.pk})
+
+    def publish(self):
+        self.published_on = timezone.now()
 
     def __str__(self):
         return self.title_original
